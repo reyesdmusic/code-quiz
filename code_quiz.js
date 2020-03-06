6eafd30a7eb059e5077e700b3422e34d7e11b103
Danny Reyes <reyesdmusic@gmail.com>
2020-03-06 13:07:57 -0500

    all questions done, start again button works now, working on comparing user answer to correct answers

--- code_quiz.js
+++ code_quiz.js
@@ -1,14 +1,27 @@
 
  $(document).ready(function() {
 
-    var i = 0;
-    
+  
+    var i;
+  
+    var answerA = ["A javaScript library.", "Connecting to a server.", "Terminal", "Copy", "<>", "Disregard Order Message", "%", "DOM", "The content looks good in most viewports, regardless of size.", "*"];
+    var answerB = ["An API.", "Factory restart.", "Method", "Scroll", "::", "Days Over Minutes", "#", "Dividend", "The application functions without internet connection.", "$"];
+    var answerC = ["A database.", "Styling HTML.", "Single", "Hover", "{}", "Document Object Model", "thisID*", "Element", "The user is expected to respond to a series of prompts.", "@"];
+    var answerD = ["A question in Java.", "Mathematical applications.", "Round", "Click", "[]", "Data Ordering Model", "$", "Compartment", "Everything that is written is converted to audio wave files.", "&"];
+    var question = ["What is jQuery?", "What is CSS used for?", "What is the term for a function within an object?", "Which of the following is not an example of an event.", "In javaScript, which kind of enclosure is used in an array?", "What do the letters in DOM stand for?", "In CSS, which of the following denotes an ID?", "A <div> is an example of an HTML ___________ .", "What is a common feature of a responsive layout?", "Which symbol is most commonly found as the intial character in code related to jQuery?"];
 
-    var answerA = ["A javaScript library.", "Connecting to a server.", "Terminal", "Copy"];
-    var answerB = ["An API.", "Factory restart.", "Method", "Scroll"];
-    var answerC = ["A database.", "Styling HTML.", "Single", "Hover"];
-    var answerD = ["A question in Java.", "Mathematical applications.", "Round", "Click"];
-    var question = ["What is jQuery?", "What is CSS used for?", "What is the term for a function within an object?", "Which of the following is not an example of an event."];
+    var correctAnswer = [1, 3, 2, 1, 4, 3, 2, 3, 1, 2];
+    var userAnswer = [];
+
+    //hidden div
+
+
+    $("#questions-prompt").hide();
+    $("#high-scores-prompt").hide();
+    $("#save-score-prompt").hide();
+
+
+    //function to cycle through questions and answers
 
 
     function nextQuestion() {
@@ -20,34 +33,20 @@
     $("#checkbox-c").text(answerC[i]);
     $("#checkbox-d").text(answerD[i]);
     }
-
-    $("#questions-prompt").hide();
-    $("#high-scores-prompt").hide();
-    $("#save-score-prompt").hide();
-
-    
-    
+  
 
     $("#start-quiz").on("click", function setTime() {
     
-    $("#starting-prompt").hide();
-    $("#questions-prompt").show();
-
-    nextQuestion();
+        $("#starting-prompt").hide();
+        $("#questions-prompt").show();
+        i = 0;
+        nextQuestion();
    
 
-    $(".question-button").on("click", function newQuestion() {
-        i++; 
-       nextQuestion();
-
-       if (i === answerA.length) {
-        $("#questions-prompt").hide();
-        $("#save-score-prompt").show(); 
-    }
-    });
+   
 
 
-    var timeLeft = 60;
+    var timeLeft = 100;
   
     var timeInterval = setInterval(function() {
       $("#time-left").text(timeLeft + " Seconds");
@@ -56,12 +55,61 @@
       if (timeLeft === -1) {
         clearInterval(timeInterval);
         $("#questions-prompt").hide();
-        $("#save-score-prompt").show(); 
+        $("#save-score-prompt").show();
       }
-  
+
+      if (i === answerA.length) {
+        clearInterval(timeInterval); 
+        timeLeft = 0;
+        $("#time-left").text(timeLeft + " Seconds");
+        $("#questions-prompt").hide();
+        $("#save-score-prompt").show();
+      
+    }
+
     }, 1000);
+  
+  
   });
 
+  $(".question-button").on("click", function newQuestion() {
+      
+    i++; 
+
+   nextQuestion();
+
+
+});
+
+$("#start-again-button").on("click", function newGame() {
+  i = 0;
+  $("#starting-prompt").show();
+  $("#save-score-prompt").hide();
+  clearInterval(timeInterval); 
+
+});
+
+$("#checkbox-a").on("click", function logLetter() {
+  userAnswer.push(1);
+  console.log(userAnswer);
+  x = userAnswer.length - 1;
+  console.log(x);
+ })
+
+ $("#checkbox-b").on("click", function logLetter() {
+  userAnswer.push(2);
+  console.log(userAnswer);
+ })
+
+ $("#checkbox-c").on("click", function logLetter() {
+  userAnswer.push(3);
+  console.log(userAnswer);
+ })
+
+ $("#checkbox-d").on("click", function logLetter() {
+  userAnswer.push(4);
+  console.log(userAnswer);
+ })
 
 
  });
